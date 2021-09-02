c63c77bf532a09343aa276c4e4de8f9803adf0d5
stromjust <justas.stromilas@gmail.com>
2021-09-02 01:12:39 +0100

    Create base API, router

--- frontend/critter/src/App.js
+++ frontend/critter/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import { CssBaseline } from '@material-ui/core'
 
-function App() {
+const App = () => {
   return (
     <div>
-      
+      <CssBaseline />
+      <Router>
+        <Route path="/" exact>
+          <Dashboard />
+          <Redirect to="/signin" />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+      </Router>
     </div>
   )
 }
---
+++ frontend/critter/src/core/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios' 
+
+const BASE_URL = process.env.REACT_APP_BACKEND_URL
+console.log(BASE_URL)
+export const api = axios.create({
+  baseURL: BASE_URL,
+})
+
+export default api
