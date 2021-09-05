f6ae00cfce779897f3a58ede45b162c7ce656c26
stromjust <justas.stromilas@gmail.com>
2021-09-05 01:48:35 +0100

    ✔ Added create post component

--- frontend/critter/src/components/CreatePost.js
+++ frontend/critter/src/components/CreatePost.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, Typography, Divider, TextField, Stack, Avatar, Button } from '@material-ui/core'
+import { useSelector } from 'react-redux'
 
-const CreatePost = () => {
+const CreatePost = (props) => {
+  const [text, setText] = useState('')
+  const user = useSelector(state => state.auth?.user)
+
+  const handleSubmit = () => {
+    props.onSubmit(text)
+  }
 
   return (
-    <></>
+    <Card>
+      <Typography>What's on your mind? 🥜</Typography>
+      <Divider sx={{ my: 1 }} />
+      <Stack direction="row">
+        <Avatar
+          sx={{
+            backgroundColor: 'primary.main',
+            mr: 1,
+          }}
+          alt={user.name}
+          variant="rounded-m"
+        >
+          {user?.name[0]}
+        </Avatar>
+        <TextField
+          variant="standard"
+          fullWidth
+          multiline
+          rows={3}
+          placeholder="Share your thoughts"
+          onChange={(e) => setText(e.target.value)}
+        />
+      </Stack>
+      <Stack direction='row-reverse' sx={{ pt: 1 }}>
+        <Button variant='contained' onClick={handleSubmit}>
+          Post
+        </Button>
+      </Stack>
+    </Card>
   )
 }
+
+export default CreatePost
