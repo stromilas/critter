8baeb43f8674f17b1d668eb5933bcf4bc07958c1
stromjust <justas.stromilas@gmail.com>
2021-09-05 01:49:04 +0100

    ✔ Added post submitting

--- frontend/critter/src/pages/Home.js
+++ frontend/critter/src/pages/Home.js
@@ -1,50 +1,62 @@
+import React, { useEffect, useState } from 'react'
 import { Card, Container, Stack } from '@material-ui/core'
 import { useSelector } from 'react-redux'
-import React, { useEffect, useState } from 'react'
+import CreatePost from '../components/CreatePost'
 import api from '../core/api'
+import Post from '../components/Post'
 
 const Home = () => {
   const [posts, setPosts] = useState([])
-  const authenticated = useSelector(state => state.auth.authenticated)
+  const authenticated = useSelector((state) => state.auth.authenticated)
 
   useEffect(() => {
-    api.get('posts')
-      .then(res => {
-        console.log(res.data);
+    api
+      .get('posts')
+      .then((res) => {
+        console.log(res.data)
         setPosts(res.data?.posts ?? [])
       })
-      .catch(e => console.error(e))
+      .catch((e) => console.error(e))
   }, [])
+
+  const submitPost = (text) => {
+    const body = {
+      post: {
+        text,
+      }
+    }
+    api
+      .post('posts', body)
+      .then((res) => {
+        console.log(res)
+      })
+      .catch((e) => {
+        console.warn(e)
+      })
+  }
 
   return (
     <Container sx={{ mt: 5 }}>
-      <Stack direction='row' spacing={5}>
-        {/* Main Feed */}
-        <Stack flexGrow='1' spacing={5}>
-          {/* New Post */}
-          { authenticated && (
-            <Card>
-              New Post
-            </Card>
-          )}
-          {/* Feed */}
-          { posts.map(post => (
-            <Card key={post.id}>:)</Card>
+      <Stack direction="row" spacing={5}>
+        {/* Feed */}
+        <Stack flexGrow="1" spacing={5}>
+          {authenticated && <CreatePost onSubmit={submitPost} />}
+          {posts.map((post) => (
+            <Post key={post.id} {...post} />
           ))}
         </Stack>
 
-        {/* Side Feed */}
-        <Stack sx={{ 
-          width: 300,
-          display: {
-            xs: 'none',
-            sm: 'flex'
-          }
-        }}>
-          {/* Trending Hashtags */}
-          <Card>
-            Trend
-          </Card>  
+        {/* Side */}
+        <Stack
+          sx={{
+            width: 300,
+            display: {
+              xs: 'none',
+              sm: 'flex',
+            },
+          }}
+        >
+          <Card sx={{p: 5}}>Trend</Card>
         </Stack>
       </Stack>
     </Container>
