2d587db6f449904033db79921682f49a40409a70
stromjust <justas.stromilas@gmail.com>
2021-09-12 17:30:39 +0100

    ♻ Refactorted auth

--- frontend/critter/src/App.js
+++ frontend/critter/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
 import { CssBaseline, Tab, Tabs } from '@material-ui/core'
 import { ThemeProvider, createTheme } from '@material-ui/core'
@@ -6,10 +6,21 @@ import themeOptions from './theme/main'
 import Header from './components/Header'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
-
+import PostPage from './pages/PostPage'
+import api from './core/api'
+import { useSelector } from 'react-redux'
 
 const App = () => {
   const theme = createTheme(themeOptions)
+  const tabPaths = ['/', '/explore', '/bookmarks']
+
+  api.interceptors.request.use((config) => {
+    const token = JSON.parse(localStorage.getItem('authState'))?.token?.access_token
+    console.log(localStorage.getItem('authState'));
+    console.log(token);
+    config.headers.Authorization = token ? `Bearer ${token}` : ''
+    return config
+  })
 
   return (
     <ThemeProvider theme={theme}>
@@ -18,10 +29,15 @@ const App = () => {
           <CssBaseline />
           <Header>
             <Route
-              path={['/', '/explore', '/bookmarks']}
-              exact
+              path={tabPaths}
               render={({ location }) => (
-                <Tabs value={location.pathname}>
+                <Tabs
+                  value={
+                    tabPaths.includes(location.pathname)
+                      ? location.pathname
+                      : tabPaths[0]
+                  }
+                >
                   <Tab
                     label="Home"
                     value="/"
@@ -50,8 +66,11 @@ const App = () => {
           <Route path="/" exact>
             <Home />
           </Route>
-          <Route path="/explore">pooopppee</Route>
-          <Route path="/bookmarks">poop</Route>
+          <Route path="/posts/:id">
+            <PostPage />
+          </Route>
+          <Route path="/explore">WIP</Route>
+          <Route path="/bookmarks">WIP</Route>
           <Route path="/login">
             <SignIn />
           </Route>
--- frontend/critter/src/store/auth.js
+++ frontend/critter/src/store/auth.js
@@ -1,30 +1,33 @@
 import { createSlice } from '@reduxjs/toolkit'
 import api from '../core/api'
 
-const setInterceptor = token => {
-  return api.interceptors.request.use((config) => {
-    config.headers.Authorization = token ? `Bearer ${token}` : ''
-    return config
-  })
-}
+// const setInterceptor = token => {
+//   const id = api.interceptors.request.use((config) => {
+//     config.headers.Authorization = token ? `Bearer ${token}` : ''
+//     return config
+//   })
+//   // console.log('setting interceptor: ', id);
+//   return id
+// }
 
-const ejectInterceptor = id => {
-  api.interceptors.request.eject(id)
-}
+// const ejectInterceptor = id => {
+//   // console.log('ejecting interceptor: ', id);
+//   api.interceptors.request.eject(id)
+// }
 
 const persisted = JSON.parse(localStorage.getItem('authState'))
 
-let interesctorID
-if (persisted) {
-  const token = persisted.token?.access_token
-  interesctorID = setInterceptor(token)
-}
+// let interesctorID
+// if (persisted) {
+//   const token = persisted.token?.access_token
+//   interesctorID = setInterceptor(token)
+// }
 
 const initialState = persisted || {
   user: undefined,
   token: undefined,
   authenticated: false,
-  intersectorID: interesctorID,
+  // intersectorID: interesctorID,
 }
 
 const authSlice = createSlice({
@@ -35,15 +38,15 @@ const authSlice = createSlice({
       state.token = payload.token
       state.user = payload.user
       state.authenticated = true
-      state.interesctorID = setInterceptor(payload.token.access_token)
+      // state.interesctorID = setInterceptor(payload.token.access_token)
       localStorage.setItem('authState', JSON.stringify(state))
     },
     logout: (state) => {
-      ejectInterceptor(state.interesctorID)
+      // ejectInterceptor(state.interesctorID)
       state.user = undefined
       state.token = undefined
       state.authenticated = false
-      state.interesctorID = undefined
+      // state.interesctorID = undefined
       localStorage.removeItem('authState')
     }
   }
