b7fde6ebbfd3412498d06a6a28dfb05d897a35c3
stromjust <justas.stromilas@gmail.com>
2022-11-03 23:55:21 +0000

    Exclude local testing file

--- backend/test.py
+++ backend/test.py
@@ -1,6 +1,2 @@
 # from critter.core import config
 from os import environ
-
-
-# '"{\"ENVIRONMENT\":\"prod\",\"AUTH_SECRET_KEY\":\"7TdbQtr+Cqw7PY5y0bf1GMC0HTzWyUx6PIEsJjklHEDlF6WsPNxIyJntK/lVp6Ay\",\"AUTH_ALGORITHM\":\"HS256\",\"AUTH_ACCESS_TOKEN_EXPIRES\":10080,\"AWS_ACCESS_KEY\":\"AKIAZILOT4UIL45QEOJK\",\"AWS_SECRET_KEY\":\"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\",\"AWS_REGION\":\"eu-west-1\",\"DB_USER\":\"postgres\",\"DB_PASSWORD\":\"rootcritter\",\"DB_NAME\":\"critters\",\"DB_HOST\":\"localhost\",\"DB_PORT\":5432}"'
-s = '{"ENVIRONMENT":"prod","AUTH_SECRET_KEY":"7TdbQtr+Cqw7PY5y0bf1GMC0HTzWyUx6PIEsJjklHEDlF6WsPNxIyJntK/lVp6Ay","AUTH_ALGORITHM":"HS256","AUTH_ACCESS_TOKEN_EXPIRES":10080,"AWS_ACCESS_KEY":"AKIAZILOT4UIL45QEOJK","AWS_SECRET_KEY":"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","AWS_REGION":"eu-west-1","DB_USER":"postgres","DB_PASSWORD":"rootcritter","DB_NAME":"critters","DB_HOST":"localhost","DB_PORT":5432}'
